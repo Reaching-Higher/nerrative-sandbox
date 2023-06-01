7c1e3358513d08c372cfee2e42be514ea5c189c9
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-06-01 05:56:29 -0700

    Build base layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Share_Tech } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import './globals.scss'
+
+const font = Share_Tech({ weight: ['400'], subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={font.className}>
+        <main className='layout'>{children}</main>
+      </body>
     </html>
   )
 }
