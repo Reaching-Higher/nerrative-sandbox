ed797688da4f11a30844e529a5135a88340aa666
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-07-12 16:10:11 -0700

    Update global CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        display: ['var(--font-display)'],
+        body: ['var(--font-body)'],
+      },
+      colors: {
+        light: '#E1E7F4',
+        dark: '#2F333C',
+        brand: '#361964',
+        brandAccent: '#E47266',
+        accent: '#EE4769'
+      },
     },
   },
   plugins: [],
-}
+};
