712b9dbd8762e7b08aa1f9f141af31d9cfa365c3
Reaching-Higher <120233993+Reaching-Higher@users.noreply.github.com>
2023-07-21 11:16:01 -0700

    Revisions to draft (test) based on feedback

--- app/eric/communitycard.tsx
+++ app/eric/communitycard.tsx
@@ -12,117 +12,100 @@ import Link from 'next/link';
   > about 12% of the vertical space available.
   # I'm assuming its okay make these comments in general our code?
   > Lol again, please and thank you!
+
 */
 
 
 const EricCommunitySection = () => {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
+    <div className="flex items-center justify-center w-full uppercase">
       {/* 
         // ^ The width here is governed by the layout that contains all pages. 
         // ^ Use "w-full" here instead.
       */}
-      <div className="grid grid-cols-2 grid-rows-4 gap-1 w-1280 h-720">
+      <div className="grid grid-cols-5 grid-rows-4 gap-1 w-full">
         {/* 
           // ^ Also use w-full here. 
           // ^ As mentioned, h-720px looks good, but relative units will protect us better. 
         */}
-        <div className="flex flex-col col-span-2 row-span-2 p-4 bg-white justify-center-top items-left sm:col-span-2 md:col-span-1 ">
+        <div className="flex flex-col col-span-3 row-span-2 p-4 bg-white justify-center-top items-left sm:col-span-2 md:col-span-3 border-solid border-2 border-dark">
           {/* 
             // ^ This is more of a design issue, but outline-black is not visible 
             // ^ against the BG. See figma comments for more on this.
           */}
-          <h1 className="mb-4 text-4xl font-bold text-dark font-body">
+          <h1 className="mb-4 text-6xl col-span-2 max-w-[80%] text-dark font-body">
             Get Involved In The Nerrative Community
           </h1>
-          <p className="text-left text-md text-dark font-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+          <p className="text-left text-3xl text-dark max-w-[80%] font-body">
+        
+            {/* So like here I tried to use the same color in the WF 
+            for the text and from my view, the text kept disappearing 
+            so It seem slike the silver is too light on top of the white
+            I'm not sure whats going going  here 
+            So I ended up using the dark, instead.
+            */}
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
         </div>
 
-        <div className="flex flex-col justify-center p-4 bg-white items-left">
-          <h2 className="mb-4 text-2xl font-bold text-dark font-body">
-            Join A Collective
-          </h2>
-          <p className="text-sm text-left text-dark font-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-
-        <div className="flex flex-col items-center row-span-2 p-4 justify-center-top bg-darkgrey">
-          <h2 className="mb-4 text-4xl font-bold text-dark font-body">
+        <div className="flex flex-col justify-center col-span-2 p-4 bg-white items-left border-solid border-2 border-dark">
+          <h2 className="mb-4 text-4xl text-dark font-body">
             Nerrative Community Forums
           </h2>
-          <p className="text-center text-md text-dark font-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-            <p>
-              <br />
+          <p className="text-left text-md text-dark max-w-[70%] font-body normal-case">
+          Nunc augue augue, efficitur vel ipsum eget, imperdiet hendrerit tellus. 
+          <p className="text-xl text-left text-dark font-body">
+            <br /> <Link href="/whereever">View all &ensp; →</Link> 
             </p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+
           </p>
         </div>
 
-        <div className="flex flex-col items-center justify-center p-4 bg-darkgrey">
-          <h2 className="mb-4 text-2xl font-bold text-dark font-body">
-            Topic Forums
+        <div class={`flex flex-col col-span-2 row-span-1 items-left justify-left p-4  text-dark hover:text-white bg-darkgrey hover:bg-dark`}>
+          <h2 className="mb-4 text-4xl  font-body">
+            New Arrivals 
+           <p className="text-xl text-left  font-body">
+                 <br /> <Link href="/whereever">Learn More &nbsp; →</Link> 
+          </p>
+          </h2> 
+
+        </div>
+
+        <div class={`flex flex-col col-span-3 row-span-2 items-left  p-4 justify-left-top sm:col-span-2 md:col-span-3 text-dark hover:text-white bg-darkgrey hover:bg-dark`}>
+          <h2 className="mb-4 text-4xl font-body">
+            Join A Collective
           </h2>
-          <p className="text-sm text-center text-dark font-body">
-            <Link href="/whereever">Learn More</Link>
+           <p className="text-lg text-left  font-body normal-case">
+               Create or join a collective group with other <br />
+               Nerrative members and forge your path together.
           </p>
+            <p className="text-xl text-left  font-body">
+            <br /> <Link href="/whereever">Learn More &nbsp; →</Link> 
+          </p>
+
         </div>
 
-        {/*  Using the tailwind brand accent color  */}
+        <div class={`flex flex-col col-span-2 row-span-1 items-left justify-left p-4  text-dark hover:text-white bg-darkgrey hover:bg-dark`}>
+          <h2 className="text-4xl font-body">
+            Topic Forums
+            </h2>  
+            <p className="text-xl text-left font-body">
+           <br /> <Link href="/whereever">Learn More &nbsp; →</Link>
+          </p>
+              
+      
+        </div>
 
-        <div className="flex flex-col items-center justify-center p-4 bg-brandAccent">
-          <h2 className="mb-4 text-4xl font-bold text-dark font-display">
+        {/*  Hover effect  */}
+      
+        <div class={`flex flex-col col-span-2  items-left justify-left p-4  text-dark hover:text-white bg-darkgrey hover:bg-dark`}>
+          <h2 class={`mb-4 text-4xl font-body`}>
             Class Forum
           </h2>
-          <p className="text-sm text-center text-dark font-body">
-            <Link href="/whereever">Learn More</Link>
+          <p class={`text-xl text-left font-body`}>
+            <Link href="/whereever">Learn More  &nbsp; →</Link>
           </p>
-        </div>
-
-        {/*  Create a class to allow the hoover transition effect w/ pulse  here  */}
-
-        <div
-          class={`flex flex-col justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4 opacity-70 animate-pulse transition duration-2 ease-in-out hover:opacity-100`}
-        >
-          <h2 className="mb-4 text-4xl font-bold text-white font-body">
-            {/* 
-            // ^ Why is there mb here? It only pushes this off center. 
-            */}
-            <Link href="/whereever">[ New Arrivals | Join Us ]</Link>
-          </h2>
-          <div className="text-lg text-center text-white font-body">
-            {/* White space / */}
-          </div>
-          {/* 
-            // ^ I don't understand the purpose of this.
-            */}
         </div>
       </div>
     </div>
