dc62178abfbe32c9d56397e1ee11b092b7ba1fb5
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-07-25 07:31:46 -0700

    Clean up.

--- app/siam/page.tsx
+++ app/siam/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SiamCommunitySection = () => {
   return (
-    <div className='flex'>...build something...</div>
+    <div className='flex items-center justify-center'>...build something...</div>
   )
 }
 
