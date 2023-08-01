c7266a0cd1eca0f85cc39299cf862abad2587488
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-07-31 23:21:40 -0700

    Update context.

--- app/_context/app.context.tsx
+++ app/_context/app.context.tsx
@@ -10,25 +10,55 @@ import { Observable, ObservableComputed } from '@legendapp/state';
 
 enableLegendStateReact();
 
-const AppState = createContext<{
+const UserState = createContext<{
   first: Observable<string>;
   last: Observable<string>;
   email: Observable<string>;
   all: ObservableComputed<string>;
 } | null>(null);
 
-export const useContact = () => useContext(AppState)!;
+const GameState = createContext<{
+  userName: Observable<string>;
+  faction: Observable<string>;
+  classType: Observable<string>;
+  expPoints: Observable<number>;
+  inGameTime: Observable<number>;
+  learningSpeed: ObservableComputed<number>;
+} | null>(null);
+
+export const useContact = () => useContext(UserState)!;
+export const useGameState = () => useContext(GameState)!;
 
 export const AppProvider = ({ children }: { children: any }) => {
   const first = useObservable('James');
   const last = useObservable('Walker');
   const email = useObservable('test@gmail.com');
+
+  const userName = useObservable('Xan4du');
+  const faction = useObservable('Engineering');
+  const classType = useObservable('Civil Engineer');
+  const expPoints = useObservable(1000);
+  const inGameTime = useObservable(10);
+
   const all = useComputed(
     () => `${first.get()} ${last.get()} | ${email.get()}`
   );
+  const learningSpeed = useComputed(() => expPoints.get() / inGameTime.get());
+
   return (
-    <AppState.Provider value={{ first, last, email, all }}>
-      {children}
-    </AppState.Provider>
+    <UserState.Provider value={{ first, last, email, all }}>
+      <GameState.Provider
+        value={{
+          userName,
+          faction,
+          classType,
+          expPoints,
+          inGameTime,
+          learningSpeed,
+        }}
+      >
+        {children}
+      </GameState.Provider>
+    </UserState.Provider>
   );
 };
