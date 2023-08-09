2660c5bf69cd39124ff27e90c47b7e2c17036759
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-08-09 07:51:10 -0700

    Reintroduce fetch now that route is built.

--- app/james/page.tsx
+++ app/james/page.tsx
@@ -24,17 +24,17 @@ const JamesCommunitySection = async () => {
     # In the production app we would move the below to a utils folder
     # so that it can be shared throughout the app.
   */
-  // const data: Response = await fetch('http://localhost:3000/api/community-james');
-  // const cards: ILinkCard[] = await data.json();
-  // console.log('cards: ', cards);
+  const data: Response = await fetch('http://localhost:3000/api/community-james');
+  const cards: ILinkCard[] = await data.json();
+  console.log('cards: ', cards);
 
   return (
     <section className="flex-container font-body">
       <div className="page-section grid grid-flow-col grid-cols-[.6fr_.4fr] grid-rows-[.4fr_.2fr_.2fr_.2fr] gap-[10px]">
         <TitleCard />
-        {/* {cards.map((card: ILinkCard) => (
+        {cards.map((card: ILinkCard) => (
           <LinkCard card={card} />
-        ))} */}
+        ))}
       </div>
     </section>
   );
