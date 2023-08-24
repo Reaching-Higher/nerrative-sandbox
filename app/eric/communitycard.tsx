75602c801ca40a803d6b968d9104b523fc6b6c81
E. Edwards <120233993+Reaching-Higher@users.noreply.github.com>
2023-08-24 12:08:19 -0700

    Updated Community Card Revision based on feedback

--- app/eric/communitycard.tsx
+++ app/eric/communitycard.tsx
@@ -1,3 +1,4 @@
+
 import Link from 'next/link';
 
 interface ICommunityCardItem {
@@ -5,11 +6,18 @@ interface ICommunityCardItem {
   blurb?: string;
   link?: string;
   buttonText?: string;
+  classes?: string;
 }
+
+
+//temporarily fix to render the flex  - change this to point to the correct API
+
+const localaddress = 'http://localhost:3000';
+
 
 const getCardData = async () => {
   const data: Response = await fetch(
-    `${process.env.BASE_URL}api/community-james`
+    `${localaddress}/api/community-eric` //Also, updated API to reflect column pages structure. 
   );
   const cards: any = await data.json();
 
@@ -21,10 +29,10 @@ const EricCommunitySection = async () => {
 
   return (
     <div className="flex items-center justify-between w-full uppercase">
-      <div className="grid grid-cols-[.6fr_.4fr] gap-1 w-full">
-        <div className="grid grid-cols-1 grid-rows-2 gap-1">
-          <div className="flex flex-col col-span-3 row-span-1 p-4 bg-white border-solid justify-center-top items-left sm:col-span-3 md:col-span-3 border-1 border-dark">
-            <h1 className="mb-4 text-6xl col-span-2 max-w-[80%] text-dark font-body">
+      <div className="page-section grid grid-flow-col grid-cols-[.6fr_.4fr] grid-rows-[.4fr_.2fr_.2fr_.4fr] gap-1 ">
+       
+          <div className="flex flex-col gap-4 row-span-2 p-4 bg-white border-solid border-1 border-dark">
+            <h1 className="text-6xl col-span-2 max-w-[80%] text-dark font-body">
               Get Involved In The Nerrative Community
             </h1>
             <p className="text-left text-3xl text-dark max-w-[80%] font-body">
@@ -32,13 +40,14 @@ const EricCommunitySection = async () => {
             </p>
           </div>
 
-          {cardData.slice(0, 1).map((item: ICommunityCardItem, index) => {
+          {cardData.map((item: ICommunityCardItem, index:number) => {
             return (
               <div
                 key={index}
-                className={`flex flex-col col-span-3 row-span-2 p-4 justify-between-top items-left sm:col-span-3 md:col-span-3 text-dark hover:text-white bg-darkgrey hover:bg-dark border-solid border-1 border-dark`}
+                className={`flex flex-col  p-4 justify-between-top  text-dark hover:text-white bg-darkgrey hover:bg-dark border-solid border-1 border-dark ${item.classes}` } 
+                
               >
-                <h2 className="mb-4 text-4xl font-body">{item.title}</h2>
+                <h2 className="text-4xl font-body">{item.title}</h2>
                 {item.blurb && (
                   <p className="text-xl text-left font-body">{item.blurb}</p>
                 )}
@@ -53,27 +62,7 @@ const EricCommunitySection = async () => {
         </div>
 
 
-        <div className="grid grid-cols-[.6fr_.4fr] grid-rows-1 gap-1">
-          {cardData.slice(1, 6).map((item: ICommunityCardItem, index) => {
-            return (
-              <div
-                key={index}
-                className={`flex flex-col col-span-3 row-span-2 p-4 justify-center-top items-left sm:col-span-2 md:col-span-3 text-dark hover:text-white bg-darkgrey  hover:bg-dark border-solid border-1 border-dark`}
-              >
-                <h2 className="mb-4 text-4xl font-body">{item.title}</h2>
-                {item.blurb && (
-                  <p className="text-xl text-left font-body">{item.blurb}</p>
-                )}
-                {item.link && (
-                  <p className="text-xl text-left font-body">
-                    <br /> <Link href={item.link}>{item.buttonText}</Link>
-                  </p>
-                )}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+   
     </div>
   );
 };
